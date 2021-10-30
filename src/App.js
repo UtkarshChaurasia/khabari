6b7261a20866dc9bf90097aff2a90408a9f78208
UtkarshChaurasia <msdianuc07@gmail.com>
2021-10-30 23:24:01 +0530

    navbar and news components setup

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        Hello my first class based component.
+        <Navbar/>
+        <News/>
       </div>
     )
   }
---
+++ src/components/News.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+    render() {
+        return (
+            <div>
+                This is a news component.
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+                <NewsItem/>
+            </div>
+        )
+    }
+}
+
+export default News
